dc87ccaa48a3ad9140fa0d6185a16536dd5e1923
AlexCh <stovertikal@mail.ru>
2022-09-28 17:09:10 +0700

    add task 004

---
+++ task_004/2_4.py
@@ -0,0 +1,22 @@
+# Напишите программу, которая принимает на вход 2 числа. Задайте список
+# из N элементов, заполненных числами из промежутка [-N,N]. Найдите произведение
+# элементов на указанных позициях (не индексах).
+
+# in position one: 3
+#    position two: 4
+#    number of elements: 5
+# out [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5] 6
+
+position_one = int(input("Введите первую позицию: "))
+position_two = int(input("Введите вторую позицию: "))
+n = int(input("Число элементов списка: "))
+
+numbers = list()
+
+i = n * -1
+
+while i <= n:
+    numbers.append(i)
+    i += 1
+print(numbers)
+print(numbers[position_one - 1] * numbers[position_two - 1])
